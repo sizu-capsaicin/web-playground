5a1d1f069c2d11440168368d570a401690aaa652
sizu <sizu@inl.ics.keio.ac.jp>
2020-10-31 02:31:53 +0900

    make scraping script

---
+++ python/scraping/README.md
@@ -0,0 +1,14 @@
+# Python - scraping
+
+Scraping with Python.  
+
+This program check the stock of [Game&Watch](https://store-jp.nintendo.com/list/hardware-accessory/hardware/HXA_S_RAAAA.html) every 5 minutes.
+
+References:
+
+- [ニンテンドースイッチの入荷をGmailで教えてくれるbotを作ってみた。(Python スクレイピング)*無料](https://note.com/yuta_ebayer/n/n13a62b327cfc)
+- [Pythonで文字列を検索（〜を含むか判定、位置取得、カウント）](https://note.nkmk.me/python-str-search/)
+- [Pythonで現在の日時を取得して指定のフォーマットの文字列に変換する](https://tonari-it.com/python-datetime-now/)
+- [Pythonでファイルの読み込み、書き込み（作成・追記）](https://note.nkmk.me/python-file-io-open-with/)
+- [Beautiful Soup Documentation - find](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find)
+- [Pythonで一定時間ごとに処理を実行する](https://qiita.com/kurogelee/items/0e5fd8b6a1d1f169179a)
---
+++ python/scraping/scraping.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import smtplib
+import datetime
+import threading
+
+# URL to scraping
+URL = "https://store-jp.nintendo.com/list/hardware-accessory/hardware/HXA_S_RAAAA.html"
+# URL = "https://store-jp.nintendo.com/list/hardware-accessory/controller/HAC_8_JASAD.html"
+# inform which agent watch site
+HEADERS = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"}
+# keywords to check stock
+KEYWORD = 'カートに入れる'
+# log file path
+LOG_PATH = '/Users/sizu/Favorit/web-playground/python/scraping/var/scraping.log'
+
+def log(file, data):
+  now = datetime.datetime.now()
+  log = '[' + str(now) + '] ' + str(data) + '\n'
+  file.write(log)
+
+def check_stock(file):
+  # request HTML of the page
+  page = requests.get(URL, headers=HEADERS)
+  # make BeautifulSoup object
+  obj = BeautifulSoup(page.text, 'html.parser')
+
+  # get text of button and check stock of product
+  res = obj.find(class_='productDetail--buttons__button--primary js-addToCartButton')
+  log(file, 'result of obj.find(): ' + str(res))
+  if res is not None:
+    word = res.get_text()
+    log(file, 'get word: ' + str(word))
+    return (word in KEYWORD)
+  
+  return False
+
+def send_email(file):
+  # set account information
+  account = 'xxx'
+  password = 'xxx'
+  # set contents of email
+  subject = 'Game&Watch arrived'
+  text = 'subject:' + subject + '\n\n' + 'It seems that Game&Watch have arrived. ' + '\n' + URL + '\n'
+  log(file, 'text of email: ' + str(text))
+
+  # send email
+  server = smtplib.SMTP("smtp.gmail.com", port=587)
+  server.ehlo()
+  server.starttls()
+  server.login(account, password)
+  server.sendmail(account, account, text)
+
+if __name__ == "__main__":
+  with open(LOG_PATH, mode='a') as file:
+    check_result = check_stock(file)
+    log(file, 'result of check_stock(): ' + str(check_result))
+    if check_result:
+      send_email(file)
