6db9c8f05ca842bc8c83e75691757f7808da0398
sizu <sizu@inl.ics.keio.ac.jp>
2020-10-31 02:32:50 +0900

    erase unuse URL

--- python/scraping/scraping.py
+++ python/scraping/scraping.py
@@ -7,7 +7,6 @@ import threading
 
 # URL to scraping
 URL = "https://store-jp.nintendo.com/list/hardware-accessory/hardware/HXA_S_RAAAA.html"
-# URL = "https://store-jp.nintendo.com/list/hardware-accessory/controller/HAC_8_JASAD.html"
 # inform which agent watch site
 HEADERS = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"}
 # keywords to check stock
