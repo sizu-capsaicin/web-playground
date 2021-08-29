fa64384776fe3675e8cac7a5db4b154699f8e2a9
sizu <sizu@inl.ics.keio.ac.jp>
2021-08-30 03:27:28 +0900

    some fix

--- python/scraping/scraping.py
+++ python/scraping/scraping.py
@@ -7,7 +7,7 @@ import threading
 from lib import output
 
 # URL to scraping
-URL = "https://store-jp.nintendo.com/list/hardware-accessory/hardware/HXA_S_RAAAA.html"
+URL = "xxx"
 # inform which agent watch site
 HEADERS = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"}
 # keywords to check stock
